cb6a2776935f42792188036355f5bdf1c33396dd
Bernd Kaiser <bernd.kaiser@inovex.de>
2023-06-06 12:55:45 +0200

    add facts fetching

---
+++ src/facts.ts
@@ -0,0 +1,56 @@
+import { getTokenRaw } from "./stores/tokenStore";
+
+const SERVER_URL = "http://localhost:5000";
+
+export async function genericFetch<T = unknown>(url: string, options: RequestInit, maxTime: number): Promise<T> {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => {
+        controller.abort();
+    }, maxTime);
+
+    try {
+        const response = await fetch(`${SERVER_URL}${url}`, { ...options, signal: controller.signal });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = (await response.json()) as T;
+        return data;
+    } finally {
+        clearTimeout(timeout);
+    }
+}
+
+export async function requestToken(): Promise<string> {
+    const result = await genericFetch<{ token: string }>(
+        "/token",
+        {
+            method: "POST",
+            mode: "cors",
+        },
+        5000
+    );
+
+    return result.token;
+}
+
+export async function getFact(): Promise<{ fact: string }> {
+    const token = getTokenRaw();
+
+    if (token.length === 0) {
+        return Promise.reject(new Error("no token"));
+    }
+
+    return genericFetch<{ fact: string }>(
+        "/fact/",
+        {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        },
+        5000
+    );
+}
